af20a9bd04b1a662d3823a8a697d75a50c2cadae
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-11-23 14:58:01 -0800

    partial 2.1 call graph
    call graph updates

--- AppCreationScripts/Cleanup.ps1
+++ AppCreationScripts/Cleanup.ps1
@@ -59,9 +59,9 @@ Function Cleanup
     # Removes the applications
     Write-Host "Cleaning-up applications from tenant '$tenantName'"
 
-    Write-Host "Removing 'webApp' (python-django-webapp-auth-my-tenant) if needed"
-    Get-AzureADApplication -Filter "DisplayName eq 'python-django-webapp-auth-my-tenant'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
-    $apps = Get-AzureADApplication -Filter "DisplayName eq 'python-django-webapp-auth-my-tenant'"
+    Write-Host "Removing 'webApp' (python-django-webapp-call-graph) if needed"
+    Get-AzureADApplication -Filter "DisplayName eq 'python-django-webapp-call-graph'"  | ForEach-Object {Remove-AzureADApplication -ObjectId $_.ObjectId }
+    $apps = Get-AzureADApplication -Filter "DisplayName eq 'python-django-webapp-call-graph'"
     if ($apps)
     {
         Remove-AzureADApplication -ObjectId $apps.ObjectId
@@ -70,10 +70,10 @@ Function Cleanup
     foreach ($app in $apps) 
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
-        Write-Host "Removed python-django-webapp-auth-my-tenant.."
+        Write-Host "Removed python-django-webapp-call-graph.."
     }
     # also remove service principals of this app
-    Get-AzureADServicePrincipal -filter "DisplayName eq 'python-django-webapp-auth-my-tenant'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
+    Get-AzureADServicePrincipal -filter "DisplayName eq 'python-django-webapp-call-graph'" | ForEach-Object {Remove-AzureADServicePrincipal -ObjectId $_.Id -Confirm:$false}
     
 }
 
