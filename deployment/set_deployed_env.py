97cb2fe67742b78049072b2a97e0621072bf6905
idg-sam <66391663+idg-sam@users.noreply.github.com>
2021-01-04 14:19:17 -0800

    deployment files

---
+++ deployment/set_deployed_env.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+import os
+import subprocess
+import sys
+
+REQUIRED_ENV_VARS = (
+    'AZ_GROUP',
+    'AZ_LOCATION',
+    'APP_SERVICE_APP_NAME',
+    'POSTGRES_SERVER_NAME',
+    'POSTGRES_ADMIN_USER',
+    'POSTGRES_ADMIN_PASSWORD',
+    'APP_DB_NAME',
+    'DJANGO_SETTINGS_MODULE'
+)
+
+missing = []
+for v in REQUIRED_ENV_VARS:
+    if v not in os.environ:
+        missing.append(v)
+if missing:
+    print("Required Environment Variables Unset:")
+    print("\t" + "\n\t".join(missing))
+    print("Exiting.")
+    exit()
+
+SETTINGS_KEYS = (
+    'POSTGRES_SERVER_NAME',
+    'POSTGRES_ADMIN_USER',
+    'POSTGRES_ADMIN_PASSWORD',
+    'POSTGRES_HOST',
+    'APP_DB_NAME',
+    'DJANGO_SETTINGS_MODULE'
+
+)
+settings_pairs = ['{}={}'.format(k, os.getenv(k)) for k in SETTINGS_KEYS]
+
+# https://docs.microsoft.com/en-us/cli/azure/webapp/config/appsettings?view=azure-cli-latest#az-webapp-config-appsettings-set
+settings_command = [
+    'az', 'webapp', 'config', 'appsettings', 'set',
+    '--name', os.getenv('APP_SERVICE_APP_NAME'),
+    '--resource-group', os.getenv('AZ_GROUP'),
+    '--settings',
+] + settings_pairs
+
+update_settings = input('Update App Settings? [y/n]: ')
+if update_settings == 'y':
+    sys.stdout.write("Updating App Settings... ")
+    sys.stdout.flush()
+    subprocess.check_output(settings_command)
+    print("Done")
