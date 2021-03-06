a32c5c6ad0d24cb33e53ff3d059e0b1c08355929
idg-sam <66391663+idg-sam@users.noreply.github.com>
2021-03-05 22:02:52 -0800

    comments on azure.py file

--- Sample/azure.py
+++ Sample/azure.py
@@ -1,12 +1,17 @@
 from .settings import *
+# Our production settings file
+# Inherits all settings from settings.py
+# Adds extra settings, overwrites DATABASES with postgreSQL
+
+
+# this is so we can generate URLs for routes hosted by our app when it is hosted
+# on Azure or any other reverse proxy.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# replace this with your deployed app's public URL
 ALLOWED_HOSTS = ['*']
+# don't allow debug on prod
 DEBUG = False
-# USE_X_FORWARDED_HOST = True # this is so that HTTPS scheme is generated for request.get_absolute_uri (Azure is reverse proxy, Django thinks it is HTTP traffic even when actually HTTPS)
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # this is so that HTTPS scheme is generated for request.get_absolute_uri (Azure is reverse proxy, Django thinks it is HTTP traffic even when actually HTTPS)
-
-print("USING AZURE SETTINGS.")
-
-DEBUG = False
+# SSL
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
@@ -14,8 +19,9 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_REDIRECT_EXEMPT = []
 SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+
+# Our postgreSQL configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
